54c0cfc0525d6272a8b99ae7e02a345b43b80b7f
Denver-Pinto <37263968+Denver-Pinto@users.noreply.github.com>
2018-10-17 00:59:50 +0530

    Add files via upload

--- GameController.cs
+++ GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class GameController : MonoBehaviour {
     public Spawner enemyProducer;
     public GameObject playerPrefab;
@@ -9,30 +10,37 @@ public class GameController : MonoBehaviour {
     {
         var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
         player.onPlayerDeath += onPlayerDeath;
+        player.RefreshHealth();
         
     }
 
     void onPlayerDeath(PlayerScript player)
     {
         enemyProducer.SpawnEnemies(false);
-        Destroy(player.gameObject);
-      
-                Invoke("restartGame",5);
+        //Destroy(player.gameObject);
+        
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+        Invoke("restartGame",5);
    
       
     }
     void restartGame()
-    {
-        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        foreach (var enemy in enemies)
-        {
-          Destroy(enemy);
-       }
-        var playerObject = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), Quaternion.identity) as GameObject;
-        var cameraRig = Camera.main.GetComponent<CameraController>();
-        cameraRig.player = playerObject;
+    { 
+        //var playerObject = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), Quaternion.identity) as GameObject;
+        //var cameraRig = Camera.main.GetComponent<CameraController>();
+        //cameraRig.player = playerObject;
         enemyProducer.SpawnEnemies(true);
-        playerObject.GetComponent<PlayerScript>().onPlayerDeath += onPlayerDeath;
+        // playerObject.GetComponent<PlayerScript>().onPlayerDeath += onPlayerDeath;
+        // playerObject.GetComponent<PlayerScript>().Counttext = GameObject.Find("Counttext").GetComponent<Text>();
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+        player.onPlayerDeath += onPlayerDeath;
+        player.transform.position = new Vector3(0, 1, 0);
+        player.RefreshHealth();
+        player.RefreshScore();
     }
 
 }
